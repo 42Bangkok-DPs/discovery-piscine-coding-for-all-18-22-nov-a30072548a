1f49114a1e2bd43e162d7102a552c56932178293
a30072548a <67030336@kmitl.ac.th>
2024-11-21 15:03:29 +0700

    Add files via upload

---
+++ cell01/ex00/name.py
@@ -0,0 +1,5 @@
+first_name = "phuriwat"
+last_name = "yotasiri"
+whole_name = first_name + " " + last_name
+
+print (whole_name , end = "\n")
---
+++ cell01/ex01/name.py
@@ -0,0 +1,5 @@
+first_name = "phuriwat"
+last_name = "yothasiri"
+
+whole_name = first_name + " " + last_name
+print(whole_name)
