3101879db5aa5b2db95b07a0adaed8b811f2c4f6
a30072548a <67030336@kmitl.ac.th>
2024-11-22 21:56:39 +0700

    Add files via upload

---
+++ cell06/ex02/downcase_all.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import sys
+
+def downcase_it(string):
+    """Convert the given string to lowercase and return it."""
+    return string.lower()
+
+if len(sys.argv) < 2:
+    print("none")
+else:
+    for param in sys.argv[1:]:
+        print(downcase_it(param))
